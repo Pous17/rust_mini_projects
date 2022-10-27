9ed70cfbba0c993f80115e7654c8919c5857a2c3
Pous17 <116793451+Pous17@users.noreply.github.com>
2022-10-27 16:53:03 +0200

    inital commit, still some things to fix

---
+++ pick_stick_game/src/main.rs
@@ -0,0 +1,65 @@
+use std::{thread, time, io};
+use rand::Rng;
+
+fn player_input() -> String {
+    let mut play = String::new();
+    println!("Your turn:");
+
+    io::stdin()
+        .read_line(&mut play)
+        .expect("Unable to read input");
+    play
+}
+
+fn valid_input() -> i8 {
+    player_input()
+        .trim()
+        .parse()
+        .unwrap_or(0)
+}
+fn main() { 
+
+    // credit https://stackoverflow.com/a/65583826/20337705
+    std::process::Command::new("clear").status().unwrap();
+
+    println!("\nWelcome to my stick game.");
+    println!("\nEach turn you and your adversary will pick between 1 and 3 sticks.");
+    println!("\nThe player who picks the last stick loses.\n");
+    println!("\nLoading...");
+
+    thread::sleep(time::Duration::from_millis(5000));
+    let mut stick: i8 = 16;
+
+    loop {
+        std::process::Command::new("clear").status().unwrap();
+        println!("{}\n", "|".repeat(stick as usize));
+        
+        let a: i8 = valid_input();
+
+        if a >= 1 && a <= 3 {
+            stick -= a;
+            thread::sleep(time::Duration::from_millis(200));
+        } else {
+            println!("Enter a valid number");
+            thread::sleep(time::Duration::from_millis(700));
+        }
+        if stick <= 0 {
+            println!("You have lost...");
+            break;
+        }
+        
+        let adversary_play = rand::thread_rng().gen_range(1..=3);
+
+        stick -= adversary_play;
+
+        println!("Your adversary took {} sticks", adversary_play);
+
+        if stick <= 0 {
+            println!("\nYou win!");
+            break;
+        }
+
+        thread::sleep(time::Duration::from_millis(800));
+
+    }
+}
